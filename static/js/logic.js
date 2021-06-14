409abc0170c9ec73c9d4bd3cc60557e9d89ced7e
Leonides <leonidesguerra@gmail.com>
2021-06-14 10:48:53 -0600

    depth param complete

--- static/js/logic.js
+++ static/js/logic.js
@@ -3,7 +3,7 @@ let DataURL= "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week
 
 //initializing the map to show a map before filers ar used
 let myMap = L.map("mapid", {
-  center: [34, 0],
+  center: [34, 10],
   zoom: 3
 });
 
@@ -16,27 +16,51 @@ L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_toke
   accessToken: API_KEY
 }).addTo(myMap);
 
+
+
+
 d3.json(DataURL).then(function(response) {
   let data = response.features;
 
- 
+  
 
   data.forEach(d=>{
-    let location =[d.geometry.coordinates[0], d.geometry.coordinates[1]];
+    let location =[d.geometry.coordinates[1], d.geometry.coordinates[0]];
     let depth = d.geometry.coordinates[2];
-    let magnitude = parseInt(d.properties.mag)*30000;
+    let magnitude = parseInt(d.properties.mag);
     let type = d.properties.type;
     let place = d.properties.place;
+    let time = new Date(d.properties.time);
+
+    let depthColor = "";
+
+    if(depth <= 1){
+      depthColor= "#ccffcc"
+    } else if(depth <=2){
+      depthColor ="#ccff99"
+    } else if(depth <=3){
+      depthColor ="#66ff33"
+    }else if(depth <=4){
+      depthColor ="#00ff00"
+    } else{depthColor= "#339933"}
+    
+    
+  
     
     L.circle(location, {
-      fillOpacity: 0.75,
-      color: "purple",
-      fillColor: "purple",
-      // Setting our circle's radius equal to the output of our markerSize function
-      // This will make our marker's size proportionate to its population
-      radius: magnitude
-    }).bindPopup("<h1>" + type + "</h1> <hr> <h3>Location: " + place + "</h3><h3>Magnitude:"+ magnitude).addTo(myMap);
-   });
+      fillOpacity: .8,
+      color: depthColor,
+      fillColor: depthColor,
+      radius: magnitude*50000
+    }).bindPopup("<h1>" + type + "</h1> <hr> <h3>Location: " + place + "</h3><h3>Magnitude: "+ magnitude + "</h3>"+ "<h3>Time: " + time).addTo(myMap);
+   
 
+  });
+
+  
+  myMap.on('zoomend', function() {
+    generatepoints() 
+   });
+   
   
 });
